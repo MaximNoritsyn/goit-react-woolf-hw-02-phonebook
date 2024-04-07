44f25c7fdcc1355104a53e740644aa75f43210ff
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-07 15:39:25 +0300

    patterns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,10 @@ export class App extends Component {
 
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     name: '',
@@ -20,7 +20,7 @@ export class App extends Component {
     const { name, number } = this.state
     const contact = {
       id: nanoid(),
-      name: name,
+      name: name.trim(),
       number: number
     }
     console.dir(contact)
@@ -34,9 +34,21 @@ export class App extends Component {
 
   onChange = (e) => {
     const { name, value } = e.target
-    this.setState({
-      [name]: value
-    })
+
+    let pattern;
+    if (name === 'name' || name === 'filter') {
+      console.log('name')
+      pattern = new RegExp("^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ]?[a-zA-Zа-яА-Я]*)*$");
+    } else if (name === 'number') {
+      console.log('number')
+      pattern = "\\+?\\d{1,4}?([-\\.\\s]?\\(?\\d{1,3}\\)?[-\\.\\s]?\\d{1,4}[-\\.\\s]?\\d{1,4}[-\\.\\s]?\\d{1,9})?";
+    }
+
+    if (!value || !pattern || value.match(pattern)) {
+      this.setState({
+        [name]: value
+      });
+    }
   }
 
   render() {
@@ -58,7 +70,6 @@ export class App extends Component {
       <input
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         value={this.state.name}
@@ -70,7 +81,6 @@ export class App extends Component {
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={this.state.number}
