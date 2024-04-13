2fecadd255c6c2545d6ba218b3e9f8d2713ea5a4
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-13 14:29:42 +0300

    set storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,23 @@ import { ContactList } from './contacts/contact_list/list.jsx';
 export class App extends Component {
 
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: ''
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.contacts !== prevState.contacts) {
+      const contactsString = JSON.stringify(this.state.contacts)
+      localStorage.setItem('contacts', contactsString)
+    }
   }
 
   fillContacts = (contact) => {
